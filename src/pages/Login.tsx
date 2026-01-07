8e977994b53c344f6836d8aa6cb9bd079128542b
CGB <christian.blunt12@gmail.com>
2026-01-07 18:10:19 -0500

    feat(pages): implemented home page, dynamic post pages, admin views, and empty query page for error catching

---
+++ src/pages/Login.tsx
@@ -0,0 +1,19 @@
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    login();
+    navigate("/admin");
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={handleLogin}>Log In</button>
+    </div>
+  );
+}
